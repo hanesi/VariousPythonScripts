733ebf5467d2951c20e5fbb851dd3effe2343811
hanesi <ian_hanes@utexas.edu>
2019-02-06 13:30:22 -0500

    Update CombineCSVs.py

--- CombineCSVs.py
+++ CombineCSVs.py
@@ -1,14 +1,18 @@
+"""
+This script efficiently combines csv's into one file
+"""
 import shutil
 import glob
 
 #import csv files from folder
-path = r'C:\Users\hanesi\Desktop\Projects\Python\06\csv'
+path = r'PATH TO FILES'
 allFiles = glob.glob(path + "/*.csv")
 with open(path + r'\06_Output.csv', 'wb') as outfile:
     for i, fname in enumerate(allFiles):
         with open(fname, 'rb') as infile:
+            #Only take the header from the first file
             if i != 0:
-                infile.readline()  # Throw away header on all but first file
-            # Block copy rest of file from input to output without parsing
+                infile.readline()
+            # Use shutil to block copy the contents of the file to the output
             shutil.copyfileobj(infile, outfile)
             print(fname + " has been imported.")
